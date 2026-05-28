7dc84a1304725b13de5bdf32833eb67c0028a312
ObemaM <barackobama0405@gmail.com>
2026-05-28 16:22:21 +0500

    chore: rename APK and simplify call number filters

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "0.1.1"
         setProperty("archivesBaseName", "MedInfo-v$versionName")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
